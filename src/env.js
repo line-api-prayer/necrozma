2e6a76c8693b136b1e3c9c5a79ddf5f433ad7322
Naphong Chadha <naphong.c@agoda.com>
2026-01-31 20:32:37 +0700

    remove envs

--- src/env.js
+++ src/env.js
@@ -8,13 +8,6 @@ export const env = createEnv({
    */
   server: {
       NODE_ENV: z.enum(["development", "test", "production"]),
-      POSTGRES_DATABASE: z.string().min(1),
-      POSTGRES_HOST: z.string().min(1),
-      POSTGRES_PASSWORD: z.string().min(1),
-      POSTGRES_PRISMA_URL: z.string().min(1),
-      POSTGRES_URL: z.string().min(1),
-      POSTGRES_URL_NON_POOLING: z.string().min(1),
-      POSTGRES_USER: z.string().min(1),
       SUPABASE_ANON_KEY: z.string().min(1),
       SUPABASE_URL: z.string().url()
   },
@@ -34,13 +27,6 @@ export const env = createEnv({
    */
   runtimeEnv: {
       NODE_ENV: process.env.NODE_ENV,
-      POSTGRES_DATABASE: process.env.POSTGRES_DATABASE,
-      POSTGRES_HOST: process.env.POSTGRES_HOST,
-      POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD,
-      POSTGRES_PRISMA_URL: process.env.POSTGRES_PRISMA_URL,
-      POSTGRES_URL: process.env.POSTGRES_URL,
-      POSTGRES_URL_NON_POOLING: process.env.POSTGRES_URL_NON_POOLING,
-      POSTGRES_USER: process.env.POSTGRES_USER,
       SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
       SUPABASE_URL: process.env.SUPABASE_URL
   },
